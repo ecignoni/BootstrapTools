7818308599f2a3e61ae1dac517b3662f3edccf88
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2022-10-22 19:08:04 +0200

    Made installable with pip

--- bootstraptools/__init__.py
+++ bootstraptools/__init__.py
@@ -1,1 +1,1 @@
-
+from .bootstrap import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='bootstraptools',
+    version='1.0.0',
+    description='Small Python utilities to apply the bootstrap',
+    long_description=open('README.md').read(),
+    author='Edoardo Cignoni',
+    author_email='edoardo.cignoni96@gmail.com',
+    install_requires=['numpy', 'scipy'],
+)
